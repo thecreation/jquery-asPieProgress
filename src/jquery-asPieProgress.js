6786d655f00f3cb31645f3a9e7d2c4fe13cad750
amazingSurge <amazingSurge@yahoo.com>
2014-09-06 04:14:43 -0700

    update trigger func

--- src/jquery-asPieProgress.js
+++ src/jquery-asPieProgress.js
@@ -224,11 +224,10 @@
         },
         _trigger: function(eventType) {
             var method_arguments = Array.prototype.slice.call(arguments, 1),
-                data = method_arguments.concat([this]);
+                data = [this].concat(method_arguments);
             
             // event
             this.$element.trigger(pluginName + '::' + eventType, data);
-            this.$element.trigger(eventType + '.' + pluginName, data);
 
             // callback
             eventType = eventType.replace(/\b\w+\b/g, function(word) {
