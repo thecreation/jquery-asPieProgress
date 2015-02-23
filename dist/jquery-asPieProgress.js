786ca38b52208c87c5737a110763f3e0601bf021
Michal Czeraszkiewicz <czerasz.hosting@gmail.com>
2015-02-23 16:08:29 +0100

    Add files after build

--- dist/jquery-asPieProgress.js
+++ dist/jquery-asPieProgress.js
@@ -1,7 +1,18 @@
-/*! jQuery asPieProgress - v0.2.0 - 2014-12-09
+/*! jQuery asPieProgress - v0.2.0 - 2015-02-23
 * https://github.com/amazingSurge/jquery-asPieProgress
-* Copyright (c) 2014 amazingSurge; Licensed GPL */
-(function($, document, window, undefined) {
+* Copyright (c) 2015 amazingSurge; Licensed GPL */
+// Resources:
+// - [AMD wrapper](https://github.com/umdjs/umd/blob/master/jqueryPlugin.js)
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module depending on jQuery.
+        define(['jquery'], factory);
+    } else {
+        // No AMD. Register plugin with global jQuery object.
+        factory(jQuery);
+    }
+}(function ($) {
     "use strict";
 
     if (!Date.now) {
@@ -428,4 +439,4 @@
             });
         }
     };
-})(jQuery, document, window);
+}));
